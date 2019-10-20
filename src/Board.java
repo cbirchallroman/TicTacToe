e54b43ff8a6a18ebac3cd344f7d4877d4872bae9
ambitiousmustard <ambitiousmustard@gmail.com>
2019-10-20 18:18:08 -0500

    Fully integrated Board object into TicTacToe.java, replaced win conditions with those from Board object, turned buttons from array to 2D matrix

--- src/Board.java
+++ src/Board.java
@@ -5,155 +5,164 @@ import java.util.ArrayList;
 //X is computer
 enum Status{O, X, A}
 
-class Tile{
+public class Board{
 
-    private int x;
-    private int y;
-    private Tile[] tiles;
-    private ArrayList<Row> rows;
-    private Status status;
+    class Tile{
 
-    public Tile(int x, int y){
+        private int x;
+        private int y;
+        private Tile[] tiles;
+        private ArrayList<Row> rows;
+        private Board board;
+        private Status status;
 
-        this.x = x;
-        this.y = y;
-        status = Status.A;
-        rows = new ArrayList<>();
+        public Tile(int x, int y, Board board){
 
-    }
+            this.x = x;
+            this.y = y;
+            this.board = board;
+            status = Status.A;
+            rows = new ArrayList<>();
 
-    public void SetNeighbors(Tile[] tiles){
+        }
 
-        this.tiles = tiles;
+        public void SetNeighbors(Tile[] tiles){
 
-    }
+            this.tiles = tiles;
 
-    public boolean Open(){ return status == Status.A;}
-    public void Set(Status status){
+        }
 
-        if(!Open())
-            System.out.println("Can't set tile at " + x + " " + y);
+        public boolean Open(){ return status == Status.A;}
+        public void Set(Status status){
 
-        this.status = status;
-        for(Row row : rows)
-            row.updateScore(status);
+            if(!Open())
+                System.out.println("Can't set tile at " + x + " " + y);
 
-    }
+            this.status = status;
+            board.incrementTotalScore();
+            for(Row row : rows)
+                row.updateScore(status);
 
-    public String coordinates(){
+        }
 
-        return x + ", " + y;
+        public String coordinates(){
 
-    }
+            return x + ", " + y;
 
-    public String toString(){
+        }
 
-        return "[" + (status == Status.A ? ' ' : status) + "]";
+        public String toString(){
 
-    }
+            return "[" + (status == Status.A ? ' ' : status) + "]";
 
-    void JoinRow(Row row){
+        }
 
-        rows.add(row);
+        void JoinRow(Row row){
 
-    }
+            rows.add(row);
 
-    void Reset(){
+        }
 
-        status = Status.A;
+        void reset(){
 
-    }
-
-}
-
-class Row{
-
-    private Tile[] tiles;
-    private int score, absScore, target;
-
-    public Row(Tile[] tiles){
-
-        this.tiles = tiles;
-        score = 0; //negative if more X, positive if more O
-        absScore = 0; //total number of tiles claimed
-        target = tiles.length; //how many tiles of the same type necessary for win condition
-
-        //the tiles in the row will know that they are a member of this row
-        for(Tile tile : tiles)
-            tile.JoinRow(this);
-
-    }
-
-    //whether this tile is still winnable
-    //  if Math.abs(score) != absScore, this means that there are tiles of different types
-    //  therefore it is not possible to win on this row anymore
-    //  EX. a tile with two Xs and one O has a score of -1 and an abs. score of 3
-    public boolean winnable(){
-
-        return Math.abs(score) != absScore;
-
-    }
-
-    //if a player has won a row, the row must be winnable (ie. only one type of tile present)
-    //  and it must have number of claimed tiles equal to the total number of tiles
-
-    //  if the score is negative, X wins
-    //  else if score is positive, O wins
-    //  if row isn't won at all, return A as the default
-    public Status winner(){
-
-        //only proceed if conditions are met
-        if(!(winnable() && absScore == target))
-            return Status.A;
-
-        return score > 0 ? Status.O : Status.X;
-
-    }
-
-    public String toString(){
-
-        String s = "";
-        for(Tile tile : tiles)
-            s += "(" + tile.coordinates() + ") ";
-
-        return s;
-
-    }
-
-    void updateScore(Status player){
-
-        absScore++; //increase absolute score by 1
-        switch (player){
-
-            case O: //O increases score
-                score++;
-                break;
-            case X: //X decreases score
-                score--;
-                break;
+            status = Status.A;
 
         }
 
     }
 
-    void Reset(){
+    class Row{
 
-        score = 0;
-        absScore = 0;
+        private Tile[] tiles;
+        private int score, absScore, target;
+
+        public Row(Tile[] tiles){
+
+            this.tiles = tiles;
+            score = 0; //negative if more X, positive if more O
+            absScore = 0; //total number of tiles claimed
+            target = tiles.length; //how many tiles of the same type necessary for win condition
+
+            //the tiles in the row will know that they are a member of this row
+            for(Tile tile : tiles)
+                tile.JoinRow(this);
+
+        }
+
+        //whether this tile is still winnable
+        //  if Math.abs(score) != absScore, this means that there are tiles of different types
+        //  therefore it is not possible to win on this row anymore
+        //  EX. a tile with two Xs and one O has a score of -1 and an abs. score of 3
+        public boolean winnable(){
+
+            return Math.abs(score) != absScore && absScore < target;
+
+        }
+
+        //if a player has won a row, the magnitude of the score must equal the target
+        //  eg. if score is -3 or 3 and target is 3, return true
+
+        //  if the score is negative, X wins
+        //  else if score is positive, O wins
+        //  if row isn't won at all, return A as the default
+        public Status winner(){
+
+            //only proceed if conditions are met
+            if(Math.abs(score) != target)
+                return Status.A;
+
+            return score > 0 ? Status.O : Status.X;
+
+        }
+
+        public String toString(){
+
+            String s = "";
+            for(Tile tile : tiles)
+                s += "(" + tile.coordinates() + ") ";
+
+            return s;
+
+        }
+
+        void updateScore(Status player){
+
+            absScore++; //increase absolute score by 1
+            switch (player){
+
+                case O: //O increases score
+                    score++;
+                    break;
+                case X: //X decreases score
+                    score--;
+                    break;
+
+            }
+
+        }
+
+        void reset(){
+
+            score = 0;
+            absScore = 0;
+
+        }
 
     }
 
-}
-
-public class Board{
-
     private int size;
+    private int area;
+    private int totalScore;
+    private boolean winnable;
     public Tile[][] tiles;
     public Row[] rows;
 
     public Board(int size){
 
         this.size = size;
+        area = size * size;
+        totalScore = 0;
+        winnable = true;
         tiles = new Tile[size][size];   //board of size 3 has 9 tiles
         rows = new Row[size * 2 + 2];   //board of size 3 has 8 winning combinations
 
@@ -164,7 +173,7 @@ public class Board{
 
             for(int j = 0; j < size; j++){
 
-                Tile tile = new Tile(i, j);
+                Tile tile = new Tile(i, j, this);
                 tiles[i][j] = tile;
                 rowTiles[j] = tile;
 
@@ -190,11 +199,17 @@ public class Board{
         for(int k = 0; k < size; k++){
             int opposite = size - 1 - k;
             diagonal_a[k] = tiles[k][k];
-            diagonal_b[k] = tiles[opposite][opposite];
+            diagonal_b[k] = tiles[k][opposite];
         }
 
         rows[size + size] = new Row(diagonal_a);
         rows[size + size + 1] = new Row(diagonal_b);
+
+    }
+
+    public Tile getTile(int i, int j){
+
+        return tiles[i][j];
 
     }
 
@@ -211,18 +226,27 @@ public class Board{
     //returns A if no winner
     public Status winner(){
 
-        Status w = Status.A;
+        //this function also keeps track of whether the game is still winnable (regardless of empty spaces)
+        boolean winnable = false;
 
-        //if there is a winner on any of the rows
-        for(Row row : rows)
-            if(row.winner() != Status.A){
-                w = row.winner();
-                break;
-            }
+        //if there is a winner on any of the rows, return that one
+        for(Row row : rows){
+            Status winner = row.winner();
+            System.out.println(row + " " + winner.name() + " " + row.score);
+            if(winner != Status.A)
+                return winner;
+            else if(row.winnable())
+                winnable = true;
+        }
+
+        //winnable?
+        this.winnable = winnable;
 
         return Status.A;
 
     }
+
+    //
 
     //prints status of board
     public String toString(){
@@ -243,10 +267,19 @@ public class Board{
     public void reset(){
 
         for(Row row : rows)
-            row.Reset();
+            row.reset();
 
+        for(Tile[] row : tiles)
+            for(Tile tile : row)
+                tile.reset();
 
+        totalScore = 0;
+        winnable = true;
 
     }
+
+    public int getTotalScore(){ return totalScore; }
+    public void incrementTotalScore(){ totalScore++; }
+    public boolean noMovesLeft(){ return totalScore == area; }
 
 }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,15 +1,17 @@
 import javax.swing.*; // needed for JFrame, and JButton
 import java.awt.*; // needed for GridLayout, and for ActionListener
 
-import java.awt.event.ActionEvent;// these two are needed to sniff mouse clicks
+import java.awt.event.ActionEvent; // these two are needed to sniff mouse clicks
 import java.awt.event.ActionListener;
+import java.util.*; //needed for map
 
 public class TicTacToe implements ActionListener {
     int altNum = 0; // this number enables alternate choices between an 'x' and an 'o' for various clicks
     Board board;
     int size = 3;
     int area = 9;
-    JButton[] buttons;
+    JButton[][] buttons;
+    HashMap<JButton, Board.Tile> tilesDict;
     String marker = " "; // initialize the marker, an 'x' or an 'o' to null
 
     public void setButtons() {
@@ -17,13 +19,19 @@ public class TicTacToe implements ActionListener {
         JFrame frame = new JFrame("Tic Tac Toe"); // create a frame for the grid, with a label
         
         frame.setLayout(new GridLayout(size, size)); // make the grid be 3x3, a user choice can change this
-        
-        for (int i = 0; i < area; i++) {
-            buttons[i] = new JButton(); // point the buttons to button objects
-            buttons[i].setFont(new Font(Font.DIALOG, Font.PLAIN, 60)); // set the font of the squares
-            buttons[i].setText(" "); // initially the squares are set to an empty string
-            buttons[i].addActionListener(this); // enables association of clicks to the class object, in our case TicTacToe
-            frame.add(buttons[i]); // add the buttons to the frame
+
+        for(int i = 0; i < size; i ++){
+            for(int j = 0; j < size; j++){
+
+                buttons[i][j] = new JButton(); // point the buttons to button objects
+                buttons[i][j].setFont(new Font(Font.DIALOG, Font.PLAIN, 60)); // set the font of the squares
+                buttons[i][j].setText(" "); // initially the squares are set to an empty string
+                buttons[i][j].addActionListener(this); // enables association of clicks to the class object, in our case TicTacToe
+                frame.add(buttons[i][j]); // add the buttons to the frame
+
+                tilesDict.put(buttons[i][j], board.getTile(i, j));
+
+            }
         }
 
         frame.pack(); // this option seemed to speed up the gui display
@@ -35,16 +43,17 @@ public class TicTacToe implements ActionListener {
         
         JButton buttonClicked = (JButton) event.getSource(); // get source of clicks, an object will be returned, so casting is needed
         int playAgain = 10; // playAgain will store the user choice on whether a new game takes place
+        Board.Tile tile = tilesDict.get(buttonClicked);
 
-        if (this.altNum % 2 == 0 && buttonClicked.getText().equals(" ") && !gameEnds()) { 
-            buttonClicked.setText("X"); // the empty string option above is needed, if clicking on an already
-            altNum++; // clicked square is to cause nothing
-        } else {
-            if (buttonClicked.getText().equals(" ") && !gameEnds()) {
-                buttonClicked.setText("O");
-                altNum++;
-            } 
-        }
+        if(!tile.Open())
+            return;
+
+        Status claim = this.altNum % 2 == 0 ? Status.X : Status.O;
+        tile.Set(claim);
+        buttonClicked.setText(claim.name());
+        altNum++;
+
+        System.out.println(board);
 
         if (gameEnds()) { // if the game has ended
               if (altNum < area) { // before reaching 9 moves, someone has won
@@ -67,8 +76,10 @@ public class TicTacToe implements ActionListener {
     }
     public void clearButtons() {
 
-        for (int i = 0; i < area; i++) {
-            buttons[i].setText(" "); // reset the buttons to empty
+        for (int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++){
+                buttons[i][j].setText(" "); // reset the buttons to empty
+            }
         }
 
         board.reset();
@@ -77,44 +88,20 @@ public class TicTacToe implements ActionListener {
     }
     public boolean gameEnds() {
 
-        
-        for (int i = 0; i <= 6; i += 3) { // horizontal winning combinations
-            if (adjacents(i, i+1) && adjacents(i+1, i+2)) {
-                marker = buttons[i].getText();
-                return true;
-            }
-        }
-        for (int i = 0; i <= 2; i++) { // vertical winning combinations
-            if (adjacents(i, i+3) && adjacents(i+3, i + 6)) {
-                marker = buttons[i].getText();
-                return true;
-            }
-        }
-        // diagonal winning combinations
-        if (adjacents(0, 4) && adjacents(4, 8)) {
-            marker = buttons[0].getText();
+        //if the board detects a winner
+        Status winner = board.winner();
+        if(winner != Status.A) {
+            marker = winner.name();
             return true;
         }
-        if (adjacents(2, 4) && adjacents(4, 6)) {
-            marker = buttons[2].getText();
-            return true;
-        }
+
         // if <area> moves have been reached the game ends in a draw
-        if (altNum == area) {
+        if (board.noMovesLeft()) {
             return true;
         }
         // otherwise the game is still afoot
         return false;
         
-    }
-    // adjacents because of the constraint equals imposes for string checking, we can only check two args
-    public boolean adjacents(int i, int j) {
-        // we want to compare squares with text, that have already been clicked
-        if (buttons[i].getText().equals(buttons[j].getText()) && !buttons[i].getText().equals(" ")) {
-            return true;
-        } else {
-            return false;
-        }
     }
 
     public TicTacToe(int size){
@@ -123,7 +110,8 @@ public class TicTacToe implements ActionListener {
         area = size * size;
         board = new Board(size);
 
-        buttons = new JButton[area]; // create an array of size^2 buttons
+        buttons = new JButton[size][size]; // create an array of size^2 buttons
+        tilesDict = new HashMap<>();
 
     }
 
